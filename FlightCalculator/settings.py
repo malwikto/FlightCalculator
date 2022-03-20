b6f06db893f1c03a7b6246b8bdacb4936dce4316
Wiktor <wiktor1malinowski@gmail.com>
2022-03-20 13:46:48 +0100

    MIGRATIONS ADDED

--- FlightCalculator/settings.py
+++ FlightCalculator/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -127,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-API_KEY = "WQ6KtG0WHRGkmzypgM7am56c0vDpawyFFD6btWDy"
+API_KEY = config('API_KEY')
 #GET https://api.flightplandatabase.com/auth/revoke
