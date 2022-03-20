0c3a7d6a27dde61a7a13132b7feebdb1dae5f58b
Wiktor <wiktor1malinowski@gmail.com>
2022-03-20 14:21:37 +0100

    settings.py updated

--- FlightCalculator/settings.py
+++ FlightCalculator/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -130,4 +132,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 API_KEY = config('API_KEY')
+django_heroku.settings(locals())
 #GET https://api.flightplandatabase.com/auth/revoke
